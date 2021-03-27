ae6025e6353cb5a611d8a0c893b0963ab656a061
Jacopo Beragnoli <beragnoli.jacopo@studenti-ittfedifermi.edu.it>
2021-03-27 15:10:53 +0100

    Created back-end Created back-end peer to peer connection Showed user video on front-end

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,29 @@
-console.log("hello world!");
+import express from "express";
+import http from "http";
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://berachat.vercel.app"],
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket: any) => {
+  socket.emit("me", socket.id);
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data: any) => {
+    io.to(data.user).emit("callUser", {signal: data.signalData, from: data.from, name: data.name});
+  });
+
+  socket.on("answerCall", (data: any) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("server running on http://localhost:5000");
+});
--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,54 +1,104 @@
-import {
-  Link as ChakraLink,
-  Text,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
+import { Container } from '../components/Container';
+import { Box } from "@chakra-ui/react";
 import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import io from "socket.io-client";
+import Peer from "simple-peer";
+import { useEffect, useRef, useState } from 'react';
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Work in progress...
-      </Text>
+const socket = io.connect("http://localhost:5000");
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+const Index = () => {
+  const [me, setMe] = useState("");
+  const [stream, setStream]:any = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal]: any = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
 
+  const video: any = useRef();
+  const userVideo: any = useRef();
+  const connectionRef: any = useRef();
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    .then((stream) => {
+      setStream(stream);
+      video.current.srcObject = stream;
+    });
+
+    socket.on("me", (id:string)=> {
+      setMe(id);
+    });
+    socket.on("callUser", (data: any) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    })
+  }, []);
+
+  const callUser = (id: string) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream
+    });
+
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name: name
+      });
+    });
+
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream;
+    });
+
+    socket.on("callAccepted", (signal: any) => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+
+    connectionRef.current = peer;
+  }
+
+  const answerCall = () => {
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", {signal: data, to: caller});
+    });
+
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
+    connectionRef.current = peer;
+  }
+
+  const leaveCall = () => {
+    setCallEnded(true);
+    connectionRef.current.destroy();
+  }
+
+  return(
+    <Container height="100vh">
     <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+    <Box>{stream && <video playsInline muted ref={video} autoPlay />}</Box>
+    </Container>
+  );
+}
 
 export default Index
